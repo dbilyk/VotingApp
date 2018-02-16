dad191fd71b7fcb1f4d6714b0c38a9f5a2c58615
Marklar <dmitri.bilyk@gmail.com>
2018-02-16 10:26:16 -0800

    implemented core tests and state manipulation logic

--- src/core.js
+++ src/core.js
@@ -1,13 +1,34 @@
-import { List, Map } from "immutable"
+import { List, Map, fromJS } from "immutable"
 
 //sets the initial entries list
 export function setEntries(state, entries) {
   return state.set('entries', List(entries))
 }
 
+//local function to calc winners of a given tally
+function getWinners(vote) {
+  if (!vote) return []
+  const [a, b] = vote.get("pair")
+  const aVotes = vote.getIn(["tally", a], 0)
+  const bVotes = vote.getIn(["tally", b], 0)
+
+  if (aVotes > bVotes) return a
+  else if (aVotes < bVotes) return b
+  else return [a, b]
+
+}
+
 //takes top two entries and puts em to a vote
 export function next(state) {
   const entries = state.get("entries")
+    .concat(getWinners(state.get("vote")))
+
+  if (entries.size == 1) {
+    return state.remove("vote")
+      .remove("entries")
+      .set("winner", entries.first())
+
+  }
 
   return state.merge({
     vote: Map({ pair: entries.take(2) }),
@@ -16,6 +37,7 @@ export function next(state) {
 
 }
 
-export function vote() {
-  //unimplemented
+export function vote(state, item) {
+  return state.updateIn(["vote", "tally", item], 0, value => value + 1)
+
 }
--- test/core_spec.js
+++ test/core_spec.js
@@ -1,4 +1,4 @@
-import { List, Map } from "immutable"
+import { List, Map, fromJS } from "immutable"
 import { expect } from "chai"
 
 import { setEntries, next, vote } from "../src/core"
@@ -25,7 +25,7 @@ describe("application logic", () => {
 
   })
 
-  describe("next vote", () => {
+  describe("next", () => {
     it("Takes the top two entries under vote", () => {
       const state = Map({
         entries: List.of(
@@ -42,23 +42,129 @@ describe("application logic", () => {
         entries: List.of("movie3", "movie4")
       }))
     })
+
+    it("puts winner back into entries", () => {
+      const state = fromJS({
+        vote: {
+          pair: ["1", "2"],
+          tally: {
+            "1": 3,
+            "2": 2
+          }
+        },
+        entries: ["3", "4"]
+      })
+
+      const nextState = next(state)
+
+      expect(nextState).to.equal(fromJS(
+        {
+          vote: {
+            pair: ["3", "4"]
+          },
+          entries: ["1"]
+        }
+      ))
+    })
+
+    it("puts both from tied vote back into entries", () => {
+      const state = fromJS({
+        vote: {
+          pair: ["1", "2"],
+          tally: {
+            "1": 2,
+            "2": 2
+          }
+        },
+        entries: ["3", "4"]
+      })
+
+      const nextState = next(state)
+
+      expect(nextState).to.equal(fromJS(
+        {
+          vote: {
+            pair: ["3", "4"]
+          },
+          entries: ["1", "2"]
+        }
+      ))
+
+
+    })
+
+    it("sets the winner prop when one entry is left, removes vote and entries", () => {
+      const state = fromJS({
+        vote: {
+          pair: ["1", "2"],
+          tally: {
+            "1": 2,
+            "2": 1
+          }
+        },
+        entries: []
+
+      })
+
+      const nextState = next(state)
+
+      expect(nextState.get("winner")).to.eq("1")
+      expect(nextState.get("votes")).to.be.undefined
+      expect(nextState.get("entries")).to.be.undefined
+
+    })
+
+
+
   })
 
   //unfinished
   describe("vote", () => {
     it("creates a new tally object if none exists", () => {
       const state = Map({
-        vote: Map({ pair: List.of("1", '2') }),
-        entries: List.of("3", "4")
+        vote: Map(
+          {
+            pair: List.of("1", '2')
+          }),
+        entries: List()
       })
+
       const nextState = vote(state, "1")
 
-      expect(nextState).to.eq(map({
+      expect(nextState).to.eq(Map({
         vote: Map({
           pair: List.of("1", "2"),
           tally: Map({ "1": 1 })
-        })
+        }),
+        entries: List()
       }))
+
+    })
+
+    it("increments an existing entry", () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of("1", "2"),
+          tally: Map({
+            "1": 1,
+            "2": 1
+          })
+        })
+      })
+
+      const nextState = vote(state, "1")
+
+      expect(nextState).to.equal(
+        Map({
+          vote: Map({
+            pair: List.of("1", "2"),
+            tally: Map({
+              "1": 2,
+              "2": 1
+            })
+          })
+        })
+      )
 
     })
 
